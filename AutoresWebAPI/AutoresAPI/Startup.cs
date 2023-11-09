6f0acf28752d792d01d1b5d4b21fd085333f2d25
jacunar <josueally@gmail.com>
2023-11-08 23:16:18 -0600

    Agregado proyecto Web API de Autores

---
+++ AutoresWebAPI/AutoresAPI/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace AutoresAPI; 
+public class Startup {
+    public IConfiguration Configuration { get; }
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+    
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment()) {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllers();
+        });
+    }
+}
