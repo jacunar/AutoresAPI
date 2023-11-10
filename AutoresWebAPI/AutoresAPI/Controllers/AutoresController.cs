db854a5617addc6f9e46570ec2b51faaf199f05c
jacunar <josueally@gmail.com>
2023-11-09 22:53:46 -0600

    Agregado controlado de autores y paquete EFCore

---
+++ AutoresWebAPI/AutoresAPI/Controllers/AutoresController.cs
@@ -0,0 +1,15 @@
+﻿using AutoresAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoresAPI.Controllers;
+[ApiController]
+[Route("api/autores")]
+public class AutoresController : ControllerBase {
+    [HttpGet]
+    public ActionResult<List<Autor>> Get() {
+        return new List<Autor>() {
+            new Autor() { Id=1, Nombre="Felipe" },
+            new Autor() { Id=2, Nombre="Josue" }
+        };
+    }
+}
---
+++ AutoresWebAPI/AutoresAPI/Entidades/Autor.cs
@@ -0,0 +1,5 @@
+﻿namespace AutoresAPI.Entidades; 
+public class Autor {
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+}
